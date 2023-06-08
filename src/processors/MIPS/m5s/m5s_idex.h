a9cf65b4da0ab4822224be27051c50583b706fc8
stelioschristou <113127795+stelioschristou@users.noreply.github.com>
2023-06-08 13:10:55 +0300

    Add MIPS Pipeline Processors
    *Pipeline (32bit)
    
    *Pipeline with Branch
    Optimization (32bit)
    
    *Pipeline with Forwarding (32bit)
    
    *Pipeline with Hazard Detection Unit (32bit)
    
    *Pipeline with Forwarding and Hazard Detection Unit (32bit)
    
    *Pipeline with Forwarding, Hazard Detection Unit and Branch Optimization (32bit)

---
+++ src/processors/MIPS/m5s/m5s_idex.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "VSRTL/core/vsrtl_component.h"
+#include "VSRTL/core/vsrtl_constant.h"
+#include "VSRTL/core/vsrtl_register.h"
+
+#include "../mips.h"
+
+#include "../m5s_unopt_no_fw_hz/m5s_unopt_no_fw_hz_idex.h"
+
+namespace vsrtl {
+namespace core {
+using namespace Ripes;
+
+/**
+ * @brief The RV5S_IDEX class
+ * A specialization of the default IDEX stage separating register utilized by
+ * the rv5s_no_fw_hz processor. Storage of register read indices is added, which
+ * are required by the forwarding unit.
+ */
+template <unsigned XLEN>
+class M5S_IDEX : public MIPS_IDEX_UNOPT<XLEN> {
+public:
+  M5S_IDEX(const std::string &name, SimComponent *parent)
+      : MIPS_IDEX_UNOPT<XLEN>(name, parent) {
+    CONNECT_REGISTERED_CLEN_INPUT(reg1_idx, this->clear, this->enable);
+    CONNECT_REGISTERED_CLEN_INPUT(reg2_idx, this->clear, this->enable);
+    // We want stalling info to persist through clearing of the register, so
+    // stalled register is always enabled and never cleared.
+    CONNECT_REGISTERED_CLEN_INPUT(stalled, 0, 1);
+  }
+
+  REGISTERED_CLEN_INPUT(reg1_idx, c_MIPSRegsBits);
+  REGISTERED_CLEN_INPUT(reg2_idx, c_MIPSRegsBits);
+  REGISTERED_CLEN_INPUT(stalled, 1);
+};
+
+} // namespace core
+} // namespace vsrtl
